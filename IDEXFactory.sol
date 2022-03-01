25203c941bfcd10b9eb15810224e639a14637634
everburn-dev <24580444+everburn-dev@users.noreply.github.com>
2022-02-28 19:28:49 -0500

    Delete IDEXFactory.sol

--- IDEXFactory.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
-
-interface IDEXFactory {
-  function createPair(address tokenA, address tokenB)
-    external
-    returns (address pair);
-}
